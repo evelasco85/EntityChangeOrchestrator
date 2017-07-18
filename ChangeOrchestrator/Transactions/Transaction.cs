075f57daaa3ee3fe74a8fc66779ac53422a5677b
Dev_2 <enrico@zilverband.com>
2017-07-18 10:37:23 +0800

    Added transaction(empty) implementation

---
+++ ChangeOrchestrator/Transactions/Transaction.cs
@@ -0,0 +1,15 @@
+﻿namespace ChangeOrchestrator.Transactions
+{
+    public interface ITransaction
+    {
+        
+    }
+
+    public class AtomicTransaction : ITransaction
+    {
+    }
+
+    public class NonAtomicTransaction : ITransaction
+    {
+    }
+}
