4ecd2bc76d0ca74e4eff140b153aabe525bec39b
Dev_2 <enrico@zilverband.com>
2017-07-18 13:39:18 +0800

    Completed basic data source retrieval

--- ChangeOrchestrator/Models/DataSource.cs
+++ ChangeOrchestrator/Models/DataSource.cs
@@ -1,6 +1,57 @@
-﻿namespace ChangeOrchestrator.Models
+﻿using System;
+using System.Collections.Generic;
+using Framework.Data_Manipulation;
+
+namespace ChangeOrchestrator.Models
 {
-    public class DataSource
+    public interface IDataSource<TSearchKey, TOutput>
     {
+        IList<TOutput> GetData(TSearchKey searchKey);
+    }
+
+    public interface IDataSource<TSearchKey, TQueryObject, TOutput> : IDataSource<TSearchKey, TOutput>
+        where TQueryObject : IBaseQueryObject<TOutput>
+    {
+        DataSource<TSearchKey, TQueryObject, TOutput>.SearchKeyToSearchInputTranslation SearchKeyToSearchInputTranslator { set; }
+    }
+
+    public class DataSource<TSearchKey, TQueryObject, TOutput> : IDataSource<TSearchKey, TQueryObject, TOutput>
+        where TQueryObject : IBaseQueryObject<TOutput>
+    {
+        public delegate object SearchKeyToSearchInputTranslation(TSearchKey key);
+
+        private IBaseQueryObject<TOutput> _queryObject;
+        private SearchKeyToSearchInputTranslation _searchKeyToSearchInputTranslator;
+
+        public SearchKeyToSearchInputTranslation SearchKeyToSearchInputTranslator
+        {
+            set { _searchKeyToSearchInputTranslator = value; }
+        }
+
+        public DataSource(TQueryObject queryObject)
+        {
+            _queryObject = queryObject;
+
+            //Default translator
+            _searchKeyToSearchInputTranslator = new SearchKeyToSearchInputTranslation((key) =>
+            {
+                return key;
+            });
+        }
+
+        public IList<TOutput> GetData(TSearchKey searchKey)
+        {
+            List<TOutput> searchResults = new List<TOutput>();
+
+            if (_queryObject == null) throw new NullReferenceException("'Query Object' is not defined");
+
+            if (_searchKeyToSearchInputTranslator == null) throw new NullReferenceException("'Search key to search input' translator not defined");
+
+            _queryObject.SearchInputObject = _searchKeyToSearchInputTranslator(searchKey);
+
+            searchResults.AddRange(_queryObject.Execute());
+
+            return searchResults;
+        }
     }
 }
---
+++ ChangeOrchestrator/Models/DataSourceAggregator.cs
@@ -0,0 +1,13 @@
+﻿namespace ChangeOrchestrator.Models
+{
+    public interface IDataSourceAggregator
+    {
+        
+    }
+
+    //Observer pattern
+    //Use transaction(atomic or non-atomic?)
+    public class DataSourceAggregator : IDataSourceAggregator   
+    {
+    }
+}
