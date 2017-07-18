01b6f1cfc994d9bc74f372c2eb5ce95aa9fa76a0
Dev_2 <enrico@zilverband.com>
2017-07-18 11:27:15 +0800

    Completed basic trx implementation

--- ChangeOrchestrator/Transactions/Transaction.cs
+++ ChangeOrchestrator/Transactions/Transaction.cs
@@ -1,15 +1,55 @@
-﻿namespace ChangeOrchestrator.Transactions
+﻿using System;
+
+namespace ChangeOrchestrator.Transactions
 {
-    public interface ITransaction
+    public interface ITransactionDispatcher
     {
-        
+        void Dispatch();
     }
 
-    public class AtomicTransaction : ITransaction
+    public interface ITransaction : ITransactionDispatcher
     {
+        void Process();
+        void Commit();
     }
 
-    public class NonAtomicTransaction : ITransaction
+    public interface IAtomicTransaction : ITransaction
     {
+        void Rollback(Exception ex);
+    }
+
+    public abstract class BaseTransaction : ITransaction
+    {
+        public abstract void Dispatch();
+        public abstract void Process();
+        public abstract void Commit();
+    }
+
+    public abstract class AtomicTransaction : BaseTransaction, IAtomicTransaction
+    {
+        public override void Dispatch()
+        {
+            try
+            {
+                Process();
+            }
+            catch (Exception e)
+            {
+                Rollback(e);
+            }
+
+            Commit();
+        }
+
+        public abstract void Rollback(Exception ex);
+    }
+
+    public abstract class NonAtomicTransaction : BaseTransaction
+    {
+        public override void Dispatch()
+        {
+            Process();
+            Commit();
+        }
     }
 }
