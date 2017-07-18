cfa3f0d3b47ccb8ba8e8ea333500274cb1ea6bc2
Dev_2 <enrico@zilverband.com>
2017-07-18 17:29:14 +0800

    Added notes

--- ChangeOrchestrator/Models/DataSourceAggregator.cs
+++ ChangeOrchestrator/Models/DataSourceAggregator.cs
@@ -7,6 +7,9 @@
 
     //Observer pattern
     //Use transaction(atomic or non-atomic?)
+    //1 Entity = Multi-data sources
+    //1 Entity = Multi-data sources identifier?
+    //1 Entity = Multi-data sources search keys(logs?)
     public class DataSourceAggregator : IDataSourceAggregator   
     {
     }
